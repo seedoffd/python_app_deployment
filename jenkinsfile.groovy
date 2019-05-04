51cee42d1c0c881a235ded26faf7e2f7e762b334
seedoffd <seedoffd@gmail.com>
2019-05-04 14:57:04 -0500

    parameter added

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,14 +1,17 @@
 node {
     properties([parameters([string(defaultValue: 'IP', description: 'where to build e.g IP', name: 'ENV', trim: true)])])
     stage("Clone repo"){
-    git "git@github.com:daudmu21/flask-examples.git"
+      git "git@github.com:seedoffd/flask-examples.git"
     }
     stage("Install Requirements"){
-    sh "scp -r *  ec2-user@${ENV}:/tmp"
-    sh "ssh ec2-user@${ENV} virtualenv /tmp/venv"
-    sh "ssh ec2-user@${ENV} ./tmp/ven/activate"
-    sh "ssh ec2-user@${ENV} sudo pip install -r /tmp/requirements.txt"
-    }
+        sh "ssh ec2-user@${ENV} sudo yum install python-pip -y"
+        sh "ssh ec2-user@${ENV} sudo pip install flask"
+        sh "ssh ec2-user@${ENV} sudo pip install virtualenv"
+        sh "scp -r *   ec2-user@${ENV}:/tmp"
+        sh "ssh ec2-user@${ENV} virtualenv /tmp/venv"
+        sh "ssh ec2-user@${ENV} ./tmp/venv/bin/activate"
+        sh "ssh ec2-user@${ENV} sudo pip install -r /tmp/requirements.txt"
+     }
     stage("start python app"){
         sh "ssh ec2-user@${ENV} python /tmp/01-hello-world/hello.py"
     }
