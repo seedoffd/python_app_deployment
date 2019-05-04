1509864f158319c79f15bf9385e733cc285ed3db
seedoffd <seedoffd@gmail.com>
2019-05-04 14:29:50 -0500

    phython

---
+++ jenkinsfile.groovy
@@ -0,0 +1,14 @@
+node {
+    stage("Clone repo")
+    git "git@github.com:daudmu21/flask-examples.git"
+    }
+    stage("Install Requirements"){
+    sh "scp -r *  ec2-user@${ENV}:/tmp"
+    sh "ssh ec2-user@${ENV} virtualenv /tmp/venv"
+    sh "ssh ec2-user@${ENV} ./tmp/ven/activate"
+    sh "ssh ec2-user@${ENV} sudo pip install -r /tmp/requirements.txt"
+    }
+    stage("start python app"){
+        sh "ssh ec2-user@${ENV} python /tmp/01-hello-world/hello.py"
+    }
+}
